99b2348d8db21fcf4d97cfd7f141e093895a8b26
himanshi.n97 <himanshi.n97@gmail.com>
2020-09-07 07:10:59 +0530

    GitHub React App

---
+++ src/Component/Profile.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import DisplayTable from "./DisplayTable";
+
+const Profile = () => {
+  const [data, setData] = useState({});
+  const [username, setUsername] = useState("");
+  const [repositories, setRepositories] = useState([]);
+  //const [commits, setCommits]= useState([]);
+
+  const onChangeHandler = e => {
+    setUsername(e.target.value);
+  };
+
+  const submitHandler = async e => {
+    e.preventDefault();
+
+    const profile = await fetch(`https://api.github.com/users/${username}`);
+    const profileJson = await profile.json();
+    console.log(profileJson);
+
+    const repositories = await fetch(profileJson.repos_url);
+    const repoJson = await repositories.json();
+    console.log(repoJson);
+
+    if (profileJson) {
+      setData(profileJson);
+      setRepositories(repoJson);
+    
+    }
+
+    {/*const commits = await fetch(`https//apigithub.com/repos/${username}/${repositories}`);
+    const commitJson = await commits.json();
+    console.log(commitJson);
+
+    if(commitJson) {
+        setCommits(commitJson);
+        //setRepositories(repoJson);
+    }*/}
+
+  };
+
+  return (
+    <>
+      <div style={{ padding: 20 }}>
+        <div className="ui search">
+          <div className="ui icon input">
+            <i className="search icon"></i>
+            <input
+              className="prompt"
+              placeholder="search username here..."
+              type="text"
+              value={username}
+              onChange={onChangeHandler}
+            />
+          </div>
+
+          <button
+            className="ui primary button"
+            type="submit"
+            onClick={submitHandler}
+          >
+            <i className="github icon"></i>
+            Search
+          </button>
+          <DisplayTable data={data} repositories={repositories} />
+        </div>
+      </div>
+    </>
+  );
+};
+export default Profile;
