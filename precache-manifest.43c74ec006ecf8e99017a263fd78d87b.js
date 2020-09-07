a50f613b1e83a143beab85655554483d5a7416c9
himanshi.n97 <himanshi.n97@gmail.com>
2020-09-07 07:26:59 +0530

    Updates

---
+++ precache-manifest.43c74ec006ecf8e99017a263fd78d87b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e62aa6b509dcab38757c3dd357244a8",
+    "url": "/github-repos/index.html"
+  },
+  {
+    "revision": "01a7f3a030418f3292e0",
+    "url": "/github-repos/static/css/main.fa19943f.chunk.css"
+  },
+  {
+    "revision": "18a240aa39e6f2daac0d",
+    "url": "/github-repos/static/js/2.5fcd3c88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-repos/static/js/2.5fcd3c88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01a7f3a030418f3292e0",
+    "url": "/github-repos/static/js/main.0b53b116.chunk.js"
+  },
+  {
+    "revision": "4fa1b933bb40a42b029e",
+    "url": "/github-repos/static/js/runtime-main.41530bfb.js"
+  }
+]);
